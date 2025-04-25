20bad616ca0ee2d31d37f452b28965bb4694bd09
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-04-25 17:06:17 +0600

    completed flightTicket ui design and added some assests for this page and made two constants file for images and icons and also made image type file

---
+++ constants/icons.ts
@@ -0,0 +1,7 @@
+import arrowLeft from "@/assets/icons/arrow-left.png";
+import search from "@/assets/icons/search.png";
+
+export const icons = {
+  search,
+  arrowLeft
+};
---
+++ constants/images.ts
@@ -0,0 +1,19 @@
+import logo from "@/assets/images/logo.png";
+import Rectangle from "@/assets/images/Rectangle-2258.png";
+import Rectangle1 from "@/assets/images/Rectangle-2258-1.png";
+import Rectangle2 from "@/assets/images/Rectangle-2258-2.png";
+import Rectangle3 from "@/assets/images/Rectangle-2258-3.png";
+import Ellipse from "@/assets/images/Ellipse-327.png";
+import Ellipse1 from "@/assets/images/Ellipse-327-1.png";
+import Ellipse2 from "@/assets/images/Ellipse-327-2.png";
+
+export const images = {
+  logo,
+  Rectangle,
+  Rectangle1,
+  Rectangle2,
+  Rectangle3,
+  Ellipse,
+  Ellipse1,
+  Ellipse2,
+};
