1ea58db5e921c637a79c1b6526d7256f8a036365
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-04-29 23:09:06 +0600

    use a reuseable components

--- app/(flight)/flightTicket/order/[ticketId].tsx
+++ app/(flight)/flightTicket/order/[ticketId].tsx
@@ -11,6 +11,7 @@ import { FontAwesome5 } from "@expo/vector-icons";
 import { Picker } from "@react-native-picker/picker";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import Header2 from "@/components/Shared/Header2";
 
 const OrderPage = () => {
   const toLocations = [
@@ -35,40 +36,7 @@ const OrderPage = () => {
   ];
   return (
     <View className='w-full min-h-screen h-auto bg-white overflow-x-hidden shadow-lg  flex-col'>
-      <View className='bg-[#fbb040] p-4 flex-row justify-center relative w-full rounded-bl-[50px] min-h-[180px]'>
-        <TouchableOpacity className='absolute left-[5%] top-11'>
-          <Image source={icons?.arrowLeft} className='w-6 h-6' />
-        </TouchableOpacity>
-        <Image
-          source={images?.logo}
-          className='w-[40%] max-w-[162px] h-[46px] mt-3'
-          accessibilityLabel='Departure Away logo, stylized pink and orange circle with text Departure Away'
-          resizeMode='contain'
-        />
-        <TouchableOpacity className='flex-row gap-3 bg-white rounded-xl p-2 border border-[#F2F2F2] absolute -bottom-[55%] px-3 py-4 w-[94%] max-w-[94%] overflow-hidden z-10'>
-          <Image
-            source={images?.rectangle}
-            className='w-24 h-[110px] rounded-lg'
-            accessibilityLabel={`Seamless Flight Booking Experience Package image`}
-            resizeMode='cover'
-          />
-          <View className='flex-col flex-1'>
-            <Text className='text-[#2B2B2B] font-medium text-sm md:text-base'>
-              From $20
-            </Text>
-            <Text className='font-medium leading-tight my-1.5 text-[#828282] text-xs md:text-sm'>
-              Seamless Flight Booking Experience Package
-            </Text>
-            <View className='flex-row items-center gap-2 my-1'>
-              <Image source={images?.ellipse} className='w-5 h-5' />
-              <Text className='text-xs font-medium text-[#828282]'>
-                Skyward Bliss
-              </Text>
-            </View>
-            <Text className='text-[#FF1A5A] font-medium text-xs'>Economy</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
+      <Header2/>
       <ScrollView
         contentContainerStyle={{ minHeight: "90%", paddingBottom: 24 }}
         showsVerticalScrollIndicator={false}
